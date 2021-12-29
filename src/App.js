495b8b443472b7efc530c526a9651834db3d8058
myb12 <mohammadyasinbappy@outlook.com>
2021-12-29 11:31:35 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles/variables.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Add Responsive container component here. Render the card components inside of that component.  */}
+      <div>
+        Replace me with a responsive container component to hold the group of
+        cards!
+      </div>
+    </div>
+  );
+}
+
+export default App;
